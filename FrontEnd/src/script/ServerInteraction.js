7b8e1c166f33139ea7942833bbb68e150cfaf888
szaff <sb782396771@163.com>
2020-11-11 22:09:55 +0800

    增加了和服务器交互的方法

---
+++ FrontEnd/src/script/ServerInteraction.js
@@ -0,0 +1,23 @@
+/**
+ * @param type: (String)请求为Post还是Get
+ * @param url: (String)请求的url
+ * @param isAsynchronous: (boolean)请求是否为异步
+ * @param func: (函数)需要执行的操作
+ * @param arguments: func中所需要的参数
+ */
+function getFromServer(type, url, isAsynchronous, func, ...arguments){
+    let xmlHttp;
+    if (window.XMLHttpRequest) {
+        xmlHttp=new XMLHttpRequest();
+    }else{
+        xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange=function(){
+        if (xmlHttp.readyState===4 && xmlHttp.status===200){
+            func(arguments)
+        }
+    }
+    xmlHttp.open(type,url,isAsynchronous);
+    xmlHttp.send();
+}
+
--- FrontEnd/src/script/Store.js
+++ FrontEnd/src/script/Store.js
@@ -108,6 +108,9 @@ function image_change() {
     }, 3900);
 }
 
+function sss(){
+
+}
 
 
 
