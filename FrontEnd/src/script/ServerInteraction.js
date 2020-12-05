a21d0f26d6a5ef3a80eb2b2d4d09ae0882408d6d
Georgezxh <11812720@mail.sustech.edu.cn>
2020-12-05 20:43:40 +0800

    login

--- FrontEnd/src/script/ServerInteraction.js
+++ FrontEnd/src/script/ServerInteraction.js
@@ -12,7 +12,7 @@ function ajax(type, url, data, f) {
     } else {
         xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlHttp.onreadystatechange = function() {
+    xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
             f(xmlHttp)
         } else {
@@ -24,6 +24,46 @@ function ajax(type, url, data, f) {
     xmlHttp.send(data);
 }
 
-function parseJson() {
-    //todo
+
+function login_ajax() {
+    
+    let logname = document.getElementsByTagName('input')['Username'].value;
+    let logpass = document.getElementsByTagName('input')['Password'].value;
+
+    if (logname == "" || logpass == "" ) {
+        // var obj = document.getElementById("errorMsg");
+        alert("用户名或密码不能为空！");
+        // obj.setAttribute("class", "tip-box visiblility-show");
+    }
+    
+    var userinfo = "inAccount=" + logname + "&inPsw=" + logpass;
+
+    var url = "http://10.21.52.79:8080/test/login?name="+logname+"&password="+logpass;
+
+    xmlhttprequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+    xmlhttprequest.onreadystatechange = function () {
+        if (xmlhttprequest.readyState == 4 && xmlhttprequest.status == 200) {
+            var result = xmlhttprequest.responseText;
+            if (result!="Fail") {
+                window.location.href = "Store.html" ;
+            } else {
+                // document.getElementById("xiaoxi").innerHTML = "登录失败！";
+                alert("用户名或密码错误！")
+            }
+        }
+    }
+    xmlhttprequest.open("get", url, true);
+    xmlhttprequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttprequest.send(null); //这时才开始发送请求
+}
+
+
+
+function sign_in() {
+    let success = 1;
+    //todo 
+    if (success) {
+        window.location.href = "Store.html";//跳转
+    }
 }
